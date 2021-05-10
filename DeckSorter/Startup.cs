68fad07ed6d583595f5e67fa94cdee4258027bce
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2021-05-10 15:39:05 +0500

    Project settings

--- DeckSorter/Startup.cs
+++ DeckSorter/Startup.cs
@@ -9,7 +9,10 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using DeckSorter.Models;
 
 namespace DeckSorter
 {
@@ -25,7 +28,19 @@ namespace DeckSorter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            var shufflerClassName = Configuration.GetChildren().First(item => item.Key == "Shuffler").Value;
+            var deckStorageClassName = Configuration.GetChildren().First(item => item.Key == "DeckStorage").Value;
+            var assemblyName = typeof(Deck).Assembly.GetName().Name;
+            var namespaceName = typeof(Deck).Namespace;
+
+            var shufflerType = Type.GetType($"{namespaceName}.{shufflerClassName}, {assemblyName}");
+            var deckStorageType = Type.GetType($"{namespaceName}.{deckStorageClassName}, {assemblyName}");
+
+            services.AddSingleton(typeof(IShuffler), shufflerType);
+            services.AddSingleton(typeof(IDeckStorage), deckStorageType);
+            services.AddControllers()
+                    .AddJsonOptions(option =>
+                        option.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
